bd59f9b1373effae22dac605bb4d6a279198b708
Nabin Hamal <68802971+nabinhamal@users.noreply.github.com>
2023-11-24 10:45:28 +0545

    cors error

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from "express"
-import cors from 'cors'
-import morgan from 'morgan'
+import express from "express";
+import cors from 'cors';
+import morgan from 'morgan';
 import connect from "./database/conn.js";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import router from './router/route.js';
 
 dotenv.config();
@@ -16,41 +16,42 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-
 /**middlewares */
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://nabinauth.netlify.app', // Allow requests from this origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 app.use(morgan('tiny'));
-app.disable('x-powerde-by');//less hackoers know about our stack
+app.disable('x-powered-by'); // Less information about your stack
 
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, '../client/build')));
 
+// API routes
+app.use('/api', router);
 
-//rest api
-app.use('*', function(req,res){
-   res.sendFile(path.join(__dirname, './client/build/index.html'));
- });
+// Serve React app
+app.use('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const port = process.env.PORT || 8080;
 
 /**Http get request */
- app.get('/',(req,res)=>{
-    res.status(201).json("Home get request")
- })
- 
-/**api routes */
-app.use('/api',router)
+app.get('/', (req, res) => {
+  res.status(201).json("Home get request");
+});
 
-
- /**start serve  only we have valid connection*/
- connect().then(() =>{
-    try{
-        app.listen(port ,()=>{
-            console.log(`server connected to ${port}`)
-         })
-    }catch(errro){
-        console.log('Cannot connect to server')
-    }
- }).catch(error =>{
-    console.log("invalid database connection..")
- })
+/**start serve only if we have a valid connection */
+connect().then(() => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server connected to ${port}`);
+    });
+  } catch (error) {
+    console.log('Cannot connect to server');
+  }
+}).catch(error => {
+  console.log("Invalid database connection..");
+});
